0686b5b53bdbb5564d7c6e78c2fe3aae11e4b9f6
Faiz Albar Risi <91599317+soulking123@users.noreply.github.com>
2025-09-25 16:20:15 +0700

    added list of mp3 file for sound

---
+++ voice/voice_data.py.py
@@ -0,0 +1,12 @@
+voice_prompts = {
+    0: "Roger is online, ready to assist",
+    1: "Keyword detected, waiting for command",
+    2: "Command unclear, please repeat the command",
+    3: "Turning on the television",
+    4: "Turning off the television",
+    5: "Turning on the fan",
+    6: "Turning off the fan",
+    7: "Turning on the lamp",
+    8: "Turning off the lamp",
+    9: "Lampu"
+}
